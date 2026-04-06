64fae1237377690fe777b956567547b26b3269b6
Gonçalo <goncalo06.rafael@gmail.com>
2026-04-06 18:04:17 +0100

    QT window functional && word on window

--- main.cpp
+++ main.cpp
@@ -1,18 +1,35 @@
 #include <stdio.h>
+#include <QApplication>
+#include <QWidget>
+#include <QString>
+#include <QTimer>
+#include <QLabel>
 #include <windows.h>
+#include <thread>
+#include <mutex>
+#include <string>
+using namespace std;
 
-int main() {
+struct ClipboardData {
+    string data;
+    mutex mutex;
+};
+
+
+
+void hotKeyThread(ClipboardData* clipboardData){
     
     if(RegisterHotKey(NULL, 1, MOD_CONTROL, 'H')) { ///basicamente e a base de evento
         printf("sucesso\n");
     } else {
         printf("fail.\n");
-        return 1;
+        
     }
     MSG msg = {0};
     for(;;) {   
         if(GetMessage(&msg, NULL, 0, 0) > 0) {
             if(msg.message == WM_HOTKEY) {
+                lock_guard<mutex> lock(clipboardData->mutex);
                 printf("pressed\n");
                 Sleep(100); //interferencia com o controlH
                 INPUT input[4] = {};
@@ -33,15 +50,42 @@ int main() {
                 SendInput(4, input, sizeof(INPUT));
                 OpenClipboard(NULL);
                 //perceber problemas de lock aqui
-                printf("Clipboard: %s\n", (char*)GetClipboardData(CF_TEXT));
+                clipboardData->data = (char*)GetClipboardData(CF_TEXT);
+                printf("Clipboard: %s\n", clipboardData->data.c_str());
                 CloseClipboard();
+                
             }
         } else {
             printf("fail.\n");
             break; 
         }
+
     }
- 
- 
-    return 0;
+}
+
+
+
+int main(int argc, char *argv[]) {
+    
+    ClipboardData clipboardData;
+    QApplication app(argc, argv);
+    QLabel window;
+    window.resize(320, 240);
+    window.setWindowTitle("Qt Window Example");
+    window.show();
+    
+    thread keyMangment(hotKeyThread, &clipboardData);
+    
+    keyMangment.detach();
+
+    QTimer timer;
+    QObject::connect(&timer, &QTimer::timeout, [&clipboardData, &window](){
+        lock_guard<mutex> lock(clipboardData.mutex);
+        window.setText(QString::fromStdString(clipboardData.data));
+        window.show(); 
+    });    
+
+    timer.start(100); 
+    return app.exec();
+    
 }
